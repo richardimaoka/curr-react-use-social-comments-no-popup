5e4f6c17911cadafe40fafdd227c9377c6a6e240
richardimaoka <richard.s.imaoka@gmail.com>
2026-03-08 22:27:10 +0900

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
-import styles from "./page.module.css";
 import CommentSection from "../components/CommentSection";
 import HeaderSection from "../components/HeaderSection";
-import VideoPlayerSection from "../components/VideoPlayerSection";
 import ReactionButtonsSection from "../components/ReactionButtonsSection";
+import VideoPlayerSection from "../components/VideoPlayerSection";
+import styles from "./page.module.css";
 
 export default function Page() {
   return (
     <div className={styles.container}>
       <HeaderSection />
 
-      <main className={styles.mainContent}>
+      <main>
         <VideoPlayerSection />
-
         <ReactionButtonsSection />
-
         <CommentSection />
       </main>
     </div>
