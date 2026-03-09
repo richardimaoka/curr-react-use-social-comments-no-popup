0630bb0820c0b7cd7c6e354d8c72dd30f615bc11
richardimaoka <richard.s.imaoka@gmail.com>
2026-03-10 00:05:31 +0900

    Image

--- components/CommentList.tsx
+++ components/CommentList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image"; // Import Image component
 import { Comment } from "../lib/comments"; // Import Comment interface
 import styles from "./CommentList.module.css";
 
@@ -12,16 +13,27 @@ const CommentList: React.FC<CommentListProps> = ({ commentsPromise }) => {
   return (
     <div className={styles.commentList}>
       {comments.map((comment) => (
-                  <div key={comment.id} className={styles.commentItem}>
-                    <img src={comment.avatarSrc} alt={`${comment.username}'s avatar`} className={styles.commenterAvatar} />
-                    <div className={styles.commentContent}>
-                      <div className={styles.commentMeta}>
-                        <span className={styles.commenterUsername}>{comment.username}</span>
-                        <span className={styles.commentTimestamp}>{comment.timestamp}</span>
-                      </div>
-                      <p className={styles.commentText}>{comment.text}</p>
-                    </div>
-                  </div>      ))}
+        <div key={comment.id} className={styles.commentItem}>
+          <Image
+            src={comment.avatarSrc}
+            alt={`${comment.username}'s avatar`}
+            className={styles.commenterAvatar}
+            width={30} // Set width based on CSS
+            height={30} // Set height based on CSS
+          />
+          <div className={styles.commentContent}>
+            <div className={styles.commentMeta}>
+              <span className={styles.commenterUsername}>
+                {comment.username}
+              </span>
+              <span className={styles.commentTimestamp}>
+                {comment.timestamp}
+              </span>
+            </div>
+            <p className={styles.commentText}>{comment.text}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
